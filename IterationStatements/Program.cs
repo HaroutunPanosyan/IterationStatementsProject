aa9a7d70c72aafd432bf9bb053c8d76328b09fd8
Haroutun Panosyan <harupanosyan@gmail.com>
2023-06-23 10:17:56 -0700

    Added all the neccessary loops.

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,37 +7,39 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
+                // TODO - Read each comment and complete its instruction
                 // like the example below
 
                 //Create a List called "numbers" - DONE!
                 var numbers = new List<int>();
-            
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
-
+            var num = 0;
 
 
             // Create a do-while loop and use the template below:
-
+            do
             {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                // Hint: reference num inside of the Add method's parentheses
+                numbers.Add(num);
 
-            } // <---- While your variable is less than 100
+            } while (num < 100);// <---- While your variable is less than 100
 
 
 
             // Create a while loop
-            // <--- While num is less than 200
+            while (num < 200)// <--- While num is less than 200
             {
                 // Increment num by 1
+                num++;
                 // Then add num to the collection numbers
-                    //HINT: copy how this was done in the do while loop
+                numbers.Add(num);
+                //HINT: copy how this was done in the do while loop
 
             }
 
@@ -48,7 +50,10 @@ namespace IterationStatements
 
             // Create a foreach loop using the collection - numbers
                 //In the scope of the foreach loop, print each number in numbers
-             
+            foreach (var items in numbers)
+            {
+                Console.WriteLine(items);
+            }
 
 
             Console.WriteLine("");
@@ -61,9 +66,10 @@ namespace IterationStatements
                 // AND as long as i is greater than or equal to 0
                 // Decrement i by 1
 
-            //start for loop here
+            for (int startingNumber = 199; startingNumber > 0; startingNumber--) //start for loop here
             {
                 // place numbers[i] inside of the Console.WriteLine() method
+                Console.WriteLine($"{startingNumber}");
             }
 
             //------------End of exercise
